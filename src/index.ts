ff19b6a0443fe16cc45075f11a5811b4548cc186
Selvaganapathi_Kanakaraj <selvaganapathikanakaraj2105@gmail.com>
2024-10-19 18:56:05 +0530

    ci: first commit changes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import type { Express, Request, Response } from 'express';
+import express from 'express';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
